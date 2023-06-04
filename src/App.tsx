444b1de09cce9dd80a2080860ec30e63e09b7e07
Bishwo Bista <bishwo5bista@gmail.com>
2023-06-04 09:10:38 +0545

    yup validation

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Resume Builder</h1>
+    <h1>Resume Builder</h1>
     <Form />
     </>
   )
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,15 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";// to integreate yup with react-hook-form
+
 const Form = () => {
-  
+  const MUST_BE_CHARACTER = "Must be character";
   const schema = yup.object().shape({
-    fullName: yup.string().required(),
+
+    //.STRICT:  //strict mode is not working
+    //using regex to check if the input is character or not
+    //writing message directly in yup is not working
+    fullName: yup.string().matches(/^[a-zA-Z ]*$/, MUST_BE_CHARACTER).required(),
     email: yup.string().email().required(),
     age: yup.number().positive().integer().min(18).required(),
     password: yup.string().min(8).max(15).required(),
@@ -21,19 +26,27 @@ const Form = () => {
   const onSubmit = (data: any) => {
     console.log(data);
   };
+  
 
   return (
+    //using onsubmit instead of onchange and onclick
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="fullname" {...register("fullName")} />
-      {/* <p>{errors.fullName?.message}</p> */}
+      {/* using <p> tag on error not working  */}
+      <p>{}</p>
+      <>{errors.fullName?.message}</>
       <input type="text" placeholder="email" {...register("email")} />
+      <>{errors.email?.message}</>
       <input type="number" placeholder="age" {...register("age")} />
+      <>{errors.age?.message}</>
       <input type="password" placeholder="password" {...register("password")} />
+      <>{errors.password?.message}</>
       <input
         type="password"
         placeholder="confirm password"
         {...register("confirmPassword")}
       />
+      <>{errors.confirmPassword?.message}</>
       <input type="submit" />
     </form>
   );
